03764cff6a27830c3a5319a8b7a2c0debe954471
gnituy18 <you@hsuyuting.com>
2023-02-01 01:01:55 +0800

    Create in-progress page

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>Hello, Taiwan Voting Guide</h1>
+  <div class="flex h-screen w-screen items-center justify-center">
+    <div>
+      <img class="h-36" src="~/assets/501.svg" alt="501" />
+      <p class="mt-4 text-center text-2xl font-bold text-[#EC502B]">
+        本頁面施工中...
+      </p>
+    </div>
+  </div>
 </template>
