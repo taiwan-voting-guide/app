3c231ab79bb90af7aeca1b1630cbbaf885d3ff48
gnituy18 <you@hsuyuting.com>
2023-02-03 01:11:00 +0800

    Add Header

--- app.vue
+++ app.vue
@@ -1,10 +1,3 @@
 <template>
-  <div class="flex h-screen w-screen items-center justify-center">
-    <div>
-      <img class="h-36" src="~/assets/501.svg" alt="501" />
-      <p class="mt-4 text-center text-2xl font-bold text-[#EC502B]">
-        本頁面施工中...
-      </p>
-    </div>
-  </div>
+  <Header />
 </template>
---
+++ components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <header>
+    <object type="image/svg+xml" data="/logo.svg"></object>
+  </header>
+</template>
