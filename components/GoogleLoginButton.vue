03d5a5d39e42a56eba5e6d95e14b266087312546
gnituy18 <you@hsuyuting.com>
2023-02-06 01:08:52 +0800

    Add login page

---
+++ components/GoogleLoginButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    id="g_id_onload"
+    :data-client_id="config.public.googleClientId"
+    data-auto_prompt="false"
+    data-login_uri="https://your.domain/your_login_endpoint"
+  ></div>
+  <div
+    class="g_id_signin"
+    data-type="standard"
+    data-theme="outline"
+    data-size="large"
+    data-text="signin_with"
+    data-shape="rectangular"
+    data-logo_alignment="left"
+  ></div>
+</template>
+
+<script setup>
+useHead({
+  script: [
+    { src: "https://accounts.google.com/gsi/client", async: true, defer: true },
+  ],
+});
+const config = useRuntimeConfig();
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,1 +1,7 @@
-<template></template>
+<template>
+  <div class="flex h-screen w-screen items-center justify-center bg-slate-100">
+    <div class="rounded bg-white p-8 shadow-xl">
+      <GoogleLoginButton />
+    </div>
+  </div>
+</template>
