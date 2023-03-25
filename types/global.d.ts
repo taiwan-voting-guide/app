1a4cdc94e58bebec8f36bf1ea6751c0da2e99078
cclin <71698978+cclin-ariel@users.noreply.github.com>
2023-03-25 13:25:19 +0900

    [update]politician/[search] 下顯示政治人物個人頁

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,10 +1,14 @@
-export {};
+export { }
 
 declare global {
   interface User {
-    id: number;
-    name: string;
-    birthdate?: string;
-    avatarUrl?: string;
+    id: number
+    name: string
+    birthdate?: string
+    avatarUrl?: string
+  }
+
+  interface PoliticiansResult {
+    politicians: Array<{ name: string; birthdate: string; avatarUrl: string; sex: string }>
   }
 }
