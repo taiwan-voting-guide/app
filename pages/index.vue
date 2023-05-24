91c8eaa18b2dd28a54c6eb1c369b31b2cb37f6de
gnituy18 <you@hsuyuting.com>
2023-05-25 01:03:48 +0800

    Add search

--- pages/index.vue
+++ pages/index.vue
@@ -2,19 +2,23 @@
   <Sidebar>
     <header class="sticky top-0 w-full bg-white py-4">
       <input
+        v-model="searchText"
         placeholder="搜尋"
         type="search"
-        class="box-border h-8 w-full rounded border-primary bg-slate-100 pl-4 placeholder:text-slate-400 focus:border-2"
+        class="box-border h-8 w-full rounded border-primary bg-slate-100 px-2 placeholder:text-slate-400 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary"
       />
     </header>
-    <SidebarItem
-      v-for="tag in allTags"
-      :onClick="() => toggleTag(tag)"
-      :key="tag"
-      :activated="isTagActive(tag)"
-    >
-      {{ tag }}
-    </SidebarItem>
+    <template v-if="filterTags && filterTags.length > 0">
+      <SidebarItem
+        v-for="tag in filterTags"
+        :onClick="() => toggleTag(tag)"
+        :key="tag"
+        :activated="isTagActive(tag)"
+      >
+        {{ tag }}
+      </SidebarItem>
+    </template>
+    <div v-else class="text-slate-500">找不到標籤</div>
   </Sidebar>
   <main class="flex-1 bg-slate-100 p-2">
     <table class="border-separate border-spacing-2">
@@ -58,6 +62,16 @@ const names = ["假人_A", "假人_B", "假人_C"];
 
 const { getTags, allTags, toggleTag, isTagActive } = useTag();
 const { politicians } = usePolitician(names);
+
+const searchText = ref<string>("");
+
+const filterTags = computed(() => {
+  return searchText.value
+    ? allTags.value.filter((tag) => {
+        return tag.includes(searchText.value);
+      })
+    : allTags.value;
+});
 
 useHead({
   title: "選前大補帖",
