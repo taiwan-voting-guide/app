1823e16cced6548cc02c9e718fa49559181c56ca
gnituy18 <you@hsuyuting.com>
2023-05-31 22:27:40 +0800

    Fix error page

--- composables/useNames.ts
+++ composables/useNames.ts
@@ -1,5 +1,4 @@
 export function useNames(): Array<string> {
   const env = useEnv();
-
   return env.isDev ? ["假人 A", "假人 B", "假人 C"] : ["吳崢", "張智倫"];
 }
--- error.vue
+++ error.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex h-screen flex-col items-center justify-center">
-    <h1 class="text-xl text-slate-700">{{ error?.message }}</h1>
+    <h1 class="text-slate-700">{{ error?.message }}</h1>
+    <NuxtLink to="/">
+      <Button>回首頁</Button>
+    </NuxtLink>
   </div>
 </template>
 
