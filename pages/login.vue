d0c95346bd2415985f987818a403104567285123
許煜廷 <you@hsuyuting.com>
2023-08-31 23:55:29 +0800

    Update login validation status

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="m-4 flex flex-1 flex-col items-center justify-center">
-    <lable>
+    <label>
       <span class="m-1 after:text-red-500 after:content-['*']">Email</span>
       <input
         :readonly="loading"
@@ -8,8 +8,9 @@
         placeholder="you@example.com"
         type="email"
         class="mb-4 block h-8 w-60 rounded-md border-primary bg-slate-50 px-2 shadow-inner placeholder:text-slate-400 read-only:cursor-not-allowed read-only:text-slate-400 focus:border-transparent focus:outline-none focus:ring-1 focus:ring-primary"
+        :class="{ 'ring-1 ring-red-500': isError }"
       />
-    </lable>
+    </label>
     <ButtonPrimary :disabled="loading" :onClick="sendVerificationCode">
       <ArrowPathIcon v-if="loading" class="h-4 w-4 animate-spin" />
       <EnvelopeIcon v-else class="h-4 w-4" />
@@ -23,15 +24,23 @@ import { EnvelopeIcon, ArrowPathIcon } from '@heroicons/vue/24/outline';
 
 const email = ref<string>('');
 const loading = ref<boolean>(false);
+const isError = ref<boolean>(false);
 
 async function sendVerificationCode() {
   loading.value = true;
-  fetch('/api/send-verification-code', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email: email.value }),
-  });
+  isError.value = false;
+  try {
+    await $fetch('/api/send-verification-code', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: email.value }),
+    });
+  } catch (error) {
+    isError.value = !!error;
+    loading.value = false;
+  } finally {
+  }
 }
 </script>
