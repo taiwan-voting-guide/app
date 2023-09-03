d5b62531180aaca5f1b04ed516025e7caeb4d5ee
許煜廷 <you@hsuyuting.com>
2023-09-04 01:01:36 +0800

    Update editor

--- pages/contribute.vue
+++ pages/contribute.vue
@@ -1,17 +1,22 @@
 <template>
-  <main class="w-full grid grid-cols-2 gap-1">
-    <MonacoEditor
-      :options="option"
-      class="h-full bg-slate-100"
-      v-model="editor"
-      lang="markdown"
-    />
-    <ContentRenderer :value="preview">
-      <template #empty>
-        <p>No content found.</p>
-      </template>
-    </ContentRenderer>
-  </main>
+  <div class="flex w-full h-full flex-col bg-slate-100 p-4">
+    <header class="flex-none w-full h-36">123</header>
+    <main class="flex-1 flex justify-stretch w-full gap-4 overflow-auto">
+      <div
+        class="border-primary border flex flex-col flex-1 w-1/2 overflow-auto"
+      >
+        <div class="h-8">123</div>
+        <div ref="editorRef" class="h-full"></div>
+      </div>
+      <div class="flex flex-1 w-1/2 h-full overflow-scroll">
+        <ContentRenderer :value="preview">
+          <template #empty>
+            <p>No content found.</p>
+          </template>
+        </ContentRenderer>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -19,6 +24,9 @@ import type { ParsedContent } from '@nuxt/content/dist/runtime/types';
 
 /* @ts-ignore */
 import markdown from '@nuxt/content/transformers/markdown';
+
+const monaco = useMonaco()!;
+const editorRef = ref();
 
 const politician = useState<string>('contribute_politician', () => '');
 const tag = useState<string>('contribute_tag', () => '');
@@ -37,6 +45,22 @@ const preview = useState<ParsedContent>('contribute_preview', () => ({
   _id: '',
 }));
 
+onMounted(() => {
+  const a = monaco.editor.create(editorRef.value, {
+    language: 'markdown',
+    theme: 'vs-dark',
+    automaticLayout: true,
+    minimap: {
+      enabled: false,
+    },
+  });
+
+  a.onDidChangeModelContent(() => {
+    editor.value = a.getValue();
+  });
+  a.setValue(editor.value);
+});
+
 watchEffect(async () => {
   const { data } = await useFetch<{ content: string }>(
     `/api/get-content?politician=${politician.value}`
@@ -45,18 +69,10 @@ watchEffect(async () => {
     return;
   }
 
-  editor.value = data.value.content;
+  editor.value = getTagSection(data.value.content, tag.value);
 });
 
 watchEffect(async () => {
   preview.value = await markdown.parse(politician.value, editor.value, {});
 });
-
-const option = {
-  theme: 'vs-dark',
-  automaticLayout: true,
-  minimap: {
-    enabled: false,
-  },
-};
 </script>
--- utils/content.ts
+++ utils/content.ts
@@ -20,3 +20,19 @@ export const queryDataContent = () =>
   useAsyncData('content-/data', () =>
     queryContent('data').sort({ order: 1 }).find()
   );
+
+export const getTagSection = (content: string, tag: string): string => {
+  const substr = `\n## ${tag}\n`;
+  const i = content.indexOf(substr);
+  if (i === -1) {
+    return '';
+  }
+
+  const sectionStart = i + substr.length;
+  const sectionEnd = content.indexOf('\n## ', sectionStart);
+  if (sectionEnd === -1) {
+    return content.substring(sectionStart);
+  }
+
+  return content.substring(sectionStart, sectionEnd);
+};
