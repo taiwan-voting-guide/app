90e4a14349dbddbdb99aeb679d74c38527ea9149
許煜廷 <you@hsuyuting.com>
2023-09-18 17:23:00 +0800

    Update cache

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -62,5 +62,15 @@ export default defineNuxtConfig({
         'Cache-Control': `public, s-max-age=${contentCacheTime} max-age=${contentCacheTime}`,
       },
     },
+    '/api/get-content': {
+      headers: {
+        'Cache-Control': `public, s-max-age=${contentCacheTime} max-age=${contentCacheTime}`,
+      },
+    },
+    '/api/get-front-matter': {
+      headers: {
+        'Cache-Control': `public, s-max-age=${contentCacheTime} max-age=${contentCacheTime}`,
+      },
+    },
   },
 });
