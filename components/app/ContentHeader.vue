570e97fdebc8248b132e3c9c4cf10dcdcf906577
許煜廷 <you@hsuyuting.com>
2023-11-23 01:29:24 +0800

    Update app header

--- components/app/ContentHeader.vue
+++ components/app/ContentHeader.vue
@@ -2,17 +2,13 @@
   <Card>
     <div class="flex items-center gap-4 p-4">
       <NuxtImg
-        v-if="data?.photoURL"
-        :src="data?.photoURL"
+        :src="`/politician/${politician}.webp`"
         :alt="politician"
+        placeholder="/placeholder.svg"
         width="48"
         height="48"
-        class="h-12 w-12 rounded-full object-cover object-top"
+        class="h-12 w-12 rounded-full bg-primary/20"
       />
-      <div
-        v-else
-        class="h-12 w-12 animate-pulse rounded-full bg-slate-100"
-      ></div>
       <h1 class="text-2xl font-bold text-slate-700">
         {{ politician }}
       </h1>
@@ -22,23 +18,7 @@
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{
+const { politician } = defineProps<{
   politician: string;
 }>();
-
-type FrontMatter = {
-  photoURL?: string;
-};
-
-const data = ref<FrontMatter>();
-
-watchEffect(async () => {
-  if (!props.politician) {
-    return;
-  }
-
-  data.value = await $fetch<FrontMatter>('/api/get-front-matter', {
-    params: { politician: props.politician },
-  });
-});
 </script>
