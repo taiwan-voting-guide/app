ec266e06bf1a1c282d42e6a435de6b1c86d9e55a
許煜廷 <you@hsuyuting.com>
2023-11-24 03:50:50 +0800

    Fix anchor link

--- components/app/Content.vue
+++ components/app/Content.vue
@@ -1,42 +1,46 @@
 <template>
   <Card>
-    <div class="sticky right-4 top-36 z-10 flex items-center p-4">
-      <h2
-        class="flex w-fit rounded-md bg-primary/20 px-2 py-1 text-xl font-bold text-slate-600 backdrop-blur"
+    <div class="w-rull flex h-full flex-col gap-4 p-4">
+      <div
+        class="space-between sticky top-36 z-10 flex rounded-md backdrop-blur"
       >
-        {{ tag }}
-      </h2>
-      <div class="ml-auto flex gap-2 rounded-md p-2 backdrop-blur">
-        <span v-if="collapsedTags.has(tag)" title="展開">
-          <BarsArrowDownIcon
-            @click="collapsedTags.delete(tag)"
-            class="h-5 w-5 cursor-pointer stroke-2 text-slate-400"
-          />
-        </span>
-        <span v-if="!collapsedTags.has(tag)" title="縮小">
-          <BarsArrowUpIcon
-            @click="collapsedTags.add(tag)"
-            class="h-5 w-5 cursor-pointer stroke-2 text-slate-400"
-          />
-        </span>
-        <span title="移除">
-          <XMarkIcon
-            @click="remove(tag)"
-            class="h-5 w-5 cursor-pointer stroke-2 text-slate-400"
-          />
-        </span>
+        <h2
+          class="anchor flex w-fit rounded-md bg-primary/20 px-2 py-1 text-xl font-bold"
+        >
+          {{ tag }}
+        </h2>
+        <div class="ml-auto flex gap-2 rounded-md p-2">
+          <span v-if="collapsedTags.has(tag)" title="展開">
+            <BarsArrowDownIcon
+              @click="collapsedTags.delete(tag)"
+              class="h-5 w-5 cursor-pointer stroke-2 text-slate-400"
+            />
+          </span>
+          <span v-if="!collapsedTags.has(tag)" title="縮小">
+            <BarsArrowUpIcon
+              @click="collapsedTags.add(tag)"
+              class="h-5 w-5 cursor-pointer stroke-2 text-slate-400"
+            />
+          </span>
+          <span title="移除">
+            <XMarkIcon
+              @click="remove(tag)"
+              class="h-5 w-5 cursor-pointer stroke-2 text-slate-400"
+            />
+          </span>
+        </div>
       </div>
-    </div>
 
-    <div
-      class="prose-h4:text-md prose prose-slate px-4 pb-4 prose-h2:text-base prose-h3:text-lg prose-a:text-blue-600"
-      :class="{
-        'hide-source': !showSource,
-        'hide-author': !showAuthor,
-      }"
-      v-if="data && !collapsedTags.has(tag)"
-      v-html="data"
-    ></div>
+      <div
+        class="prose-h4:text-md prose prose-slate prose-h2:text-base prose-h3:text-lg prose-a:text-blue-600"
+        :class="{
+          'hide-source': !showSource,
+          'hide-author': !showAuthor,
+        }"
+        v-if="data && !collapsedTags.has(tag)"
+        v-html="data"
+      ></div>
+    </div>
   </Card>
 </template>
 
