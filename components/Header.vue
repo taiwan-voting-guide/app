69a2640e5adb2aa5740b336ed9d3321a3cd374fa
許煜廷 <you@hsuyuting.com>
2023-11-24 14:41:17 +0800

    Fix header z index

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="fixed inset-x-0 top-0 z-20 flex h-16 items-center justify-between bg-slate-100/80 backdrop-blur"
+    class="fixed inset-x-0 top-0 z-30 flex h-16 items-center justify-between bg-slate-100/80 backdrop-blur"
   >
     <slot />
   </header>
