8f0cab67cc97b58fc8eab4883791b87e8c18d28c
許煜廷 <you@hsuyuting.com>
2023-11-28 03:25:26 +0800

    Update docs

--- pages/docs/introduction.vue
+++ pages/docs/introduction.vue
@@ -3,7 +3,7 @@
   <main class="mx-auto flex max-w-[720px] flex-col gap-2 px-2 pb-4 pt-16">
     <DocsTabs />
     <div
-      class="prose prose-slate p-2 prose-h1:text-slate-600 prose-h2:text-slate-600 prose-a:text-blue-600"
+      class="prose prose-slate p-2 prose-h1:text-slate-600 prose-h2:text-slate-600 prose-a:text-blue-600 prose-h3:text-slate-600 prose-h4:text-slate-600 prose-h5:text-slate-600 prose-h6:text-slate-600 prose-p:text-slate-600"
       v-html="data"
     />
   </main>
