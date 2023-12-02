eccc0c6d82a17616965fc437a40b119ba3d9bcd8
許煜廷 <you@hsuyuting.com>
2023-12-02 18:29:19 +0800

    Update docs

---
+++ components/Prose.vue
@@ -0,0 +1,14 @@
+<template>
+  <Html class="scroll-smooth" />
+  <div
+    v-if="data"
+    class="prose prose-slate p-2 prose-h1:scroll-mt-32 prose-h1:text-slate-800 prose-h2:w-fit prose-h2:scroll-mt-32 prose-h2:rounded-md prose-h2:bg-primary/20 prose-h2:px-1 prose-h2:text-slate-800 prose-h3:scroll-mt-32 prose-h3:text-slate-800 prose-h4:text-slate-800 prose-h5:text-slate-800 prose-h6:text-slate-800 prose-p:text-slate-600 prose-a:text-blue-600 prose-strong:text-slate-800 prose-img:rounded-md prose-img:border-2 prose-img:shadow-md"
+    v-html="data"
+  />
+</template>
+
+<script setup lang="ts">
+const { data } = defineProps<{
+  data: string | null;
+}>();
+</script>
--- pages/docs/how-to-contribute.vue
+++ pages/docs/how-to-contribute.vue
@@ -1,11 +1,8 @@
 <template>
   <AppHeader />
-  <main class="mx-auto flex max-w-[720px] flex-col gap-2 px-4 pb-4 pt-16">
+  <main class="mx-auto flex max-w-screen-sm flex-col gap-2 px-2 pb-2 pt-16">
     <DocsTabs />
-    <div
-      class="prose prose-slate p-2 prose-h1:text-slate-800 prose-h2:w-fit prose-h2:rounded-md prose-h2:bg-primary/20 prose-h2:px-1 prose-h2:text-slate-800 prose-h3:text-slate-800 prose-h4:text-slate-800 prose-h5:text-slate-800 prose-h6:text-slate-800 prose-p:text-slate-600 prose-a:text-blue-600 prose-strong:text-slate-600 prose-img:border prose-img:border-primary prose-img:shadow-md"
-      v-html="data"
-    />
+    <Prose :data="data" />
   </main>
 </template>
 
