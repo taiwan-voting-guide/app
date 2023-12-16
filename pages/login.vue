cd5a41c2997b1178cb68f0d19d63e9c7d5ef4451
許煜廷 <you@hsuyuting.com>
2023-12-16 14:55:30 +0800

    Use input token login

--- pages/login.vue
+++ pages/login.vue
@@ -3,35 +3,63 @@
     <AppHeader />
     <main class="flex h-full w-full flex-1 items-center justify-center p-2">
       <div class="flex w-80 flex-col gap-4">
-        <div class="flex items-center gap-1">
-          <InformationCircleIcon class="h-5 w-5 stroke-2" />
+        <template v-if="!sent">
+          <div class="flex items-center gap-1">
+            <InformationCircleIcon class="h-5 w-5 stroke-2" />
 
-          請先進行Email驗證
-        </div>
-        <form
-          class="flex w-full flex-col gap-4"
-          @submit.prevent="sendVerificationCode"
-        >
-          <label>
-            <span class="after:text-red-500 after:content-['*']">Email</span>
-            <input
-              :readonly="loading"
-              v-model="email"
-              placeholder="you@example.com"
-              type="email"
-              class="w-full rounded-md border-none px-3 py-2 placeholder-slate-400 shadow"
-              :disabled="loading || sent"
-              :class="{
-                'ring-1 ring-red-500': isError,
-                'text-slate-400': sent,
-              }"
-            />
-          </label>
-          <ButtonPrimary :submit="true" :disabled="loading || sent">
-            <template v-if="sent"> 驗證信已寄出 </template>
-            <template v-else> 寄送驗證信 </template>
-          </ButtonPrimary>
-        </form>
+            請先進行Email驗證
+          </div>
+          <form
+            class="flex w-full flex-col gap-4"
+            @submit.prevent="sendVerificationCode"
+          >
+            <label>
+              <span class="after:text-red-500 after:content-['*']">Email</span>
+              <input
+                :readonly="loading"
+                v-model="email"
+                placeholder="you@example.com"
+                type="email"
+                class="w-full rounded-md border-none px-3 py-2 placeholder-slate-400 shadow"
+                :disabled="loading || sent"
+                :class="{
+                  'ring-1 ring-red-500': isError,
+                  'text-slate-400': sent,
+                }"
+              />
+            </label>
+            <ButtonPrimary :submit="true" :disabled="loading || sent">
+              <template v-if="sent"> 驗證信已寄出 </template>
+              <template v-else> 寄送驗證信 </template>
+            </ButtonPrimary>
+          </form>
+        </template>
+        <template v-else>
+          <div class="flex items-center gap-1">
+            <InformationCircleIcon class="h-5 w-5 stroke-2" />
+            請查收郵件中的驗證碼並貼上
+          </div>
+          <form
+            class="flex w-full flex-col gap-4"
+            @submit.prevent="verifyEmail"
+          >
+            <label>
+              <span class="after:text-red-500 after:content-['*']">驗證碼</span>
+              <input
+                v-model="verification_code"
+                placeholder="驗證碼"
+                type="text"
+                class="w-full rounded-md border-none px-3 py-2 placeholder-slate-400 shadow"
+              />
+            </label>
+            <ButtonPrimary
+              :submit="true"
+              :disabled="verification_code.length === 0"
+            >
+              驗證信箱
+            </ButtonPrimary>
+          </form>
+        </template>
       </div>
     </main>
   </div>
@@ -41,6 +69,7 @@
 import { InformationCircleIcon } from '@heroicons/vue/24/outline';
 
 const email = ref<string>('');
+const verification_code = ref<string>('');
 const loading = ref<boolean>(false);
 const isError = ref<boolean>(false);
 const sent = ref<boolean>(false);
@@ -70,6 +99,21 @@ async function sendVerificationCode() {
     loading.value = false;
     isError.value = !!error;
   }
+
+  sent.value = true;
+}
+
+async function verifyEmail() {
+  await navigateTo(
+    {
+      path: '/api/verify-email',
+      query: {
+        email: email.value,
+        token: verification_code.value,
+      },
+    },
+    { external: true },
+  );
 }
 
 definePageMeta({
